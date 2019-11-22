3a147a7a54bd9296f6e98c0539f6a837daceb8ff
Yik Lung Pang <yiklungpang@gmail.com>
2019-11-22 01:54:49 +0000

    Restructuring to catkin_workspace

---
+++ S2/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/yiklungpang/Benchmark/S2/src
+# Build directory: /home/yiklungpang/Benchmark/S2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
